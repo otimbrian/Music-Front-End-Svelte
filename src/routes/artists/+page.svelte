4c1ea6a2468eaac35571100d22c0af5050fb906e
Otim Brian <otims.brian@gmail.com>
2023-03-02 15:30:18 +0300

    Added artists landing page

---
+++ src/routes/artists/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Artists from './Artists.svelte';
+</script>
+
+<div class="page">
+	<Artists />
+</div>
+
+<style>
+	.page {
+		/* background-color: red; */
+		/* align-content: center; */
+		/* width: 100%;  */
+		display: flex;
+		justify-content: center;
+	}
+</style>
