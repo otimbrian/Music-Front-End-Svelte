baa870908cff94aceba1949ad05102d85c4860ff
Otim Brian <otims.brian@gmail.com>
2023-03-02 15:33:50 +0300

    Added check box component

---
+++ src/routes/upload/CheckBoxes.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let createAlbum = false;
+	export let createArtist = false;
+	export let createMusic = false;
+</script>
+
+<div class="clickable_button">
+	<div>
+		<input type="checkbox" name="Artist" id="" bind:checked={createArtist} />
+		<h1>Artist</h1>
+	</div>
+	<div>
+		<input type="checkbox" name="Album" id="" bind:checked={createAlbum} />
+		<h1>Album</h1>
+	</div>
+	<div>
+		<input type="checkbox" name="Music" id="" bind:checked={createMusic} />
+		<h1>Music</h1>
+	</div>
+</div>
+
+<style>
+	.clickable_button {
+		display: flex;
+		padding: 10px 80px;
+	}
+
+	.clickable_button div {
+		display: block;
+		padding: 50px 50px;
+	}
+</style>
