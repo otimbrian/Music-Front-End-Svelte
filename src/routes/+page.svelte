d9e9fdd10506035db3f284bf2105deeefb1812cc
Otim Brian <otims.brian@gmail.com>
2023-03-04 11:57:49 +0300

    Added landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Sidepane from './Sidepane.svelte';
+	import Player from './Player.svelte';
+	import Music from './Music.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="homepage" content="Playlists and current music" />
+</svelte:head>
+
+<section>
+	<div style="width: 100%; display: flex; justify-content: space-around">
+		<Sidepane />
+		<Music />
+		<Player />
+	</div>
+</section>
